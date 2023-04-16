3f5596d858cd72ec73412063694b5333e4ea749f
luisgarduci <112954236+luisgarduci@users.noreply.github.com>
2023-04-16 13:11:26 -0300

    revisão

---
+++ revisão js/pag02.js
@@ -0,0 +1,26 @@
+function clicar () {
+    var ispan = document.getElementById ("span1")
+    var btnoperador = document.getElementById ("operador")
+    var btnoperador = prompt("Digite o operador")
+    switch (btnoperador) {
+        case "+":
+        ispan.innerHTML = "+"
+        operador.value = "Somar"
+        break;
+        case "-":
+        ispan.innerHTML = "-"
+        operador.value = "Subtrair"
+        break;
+        case "x":
+        ispan.innerHTML = "x"
+        operador.value = "Multiplicar"
+        break;
+        case "/":
+        ispan.innerHTML = "/"
+        operador.value = "Dividir"
+        break;
+        default:
+        confirm("Digite um operador numérico!!!")
+    }
+    
+}
